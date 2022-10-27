a208efe812910e8a6c826f6b720b4dcfa2fd71fa
Trung Hau Le <107094318+hault033092@users.noreply.github.com>
2022-10-27 19:08:50 +0700

    initialized project

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Pages from './Pages'
+
+const Home = () => {
+  return (
+    <Container>
+      <Pages />
+    </Container>
+  )
+}
+
+const Container = styled.div``
+
+export default Home
---
+++ frontend/src/Pages/Pages.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Home'
+
+const Pages = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Home />} />
+    </Routes>
+  )
+}
+
+export default Pages
