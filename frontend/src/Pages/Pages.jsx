59a5096a37e0f6a68b4b25342f06363836ccafe4
Trung Hau Le <letrunghau090298@gmail.com>
2022-11-25 11:05:39 +0700

    add routes

--- frontend/src/Pages/Pages.jsx
+++ frontend/src/Pages/Pages.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import AccountInfo from './AccountInfo'
 import Home from './Home'
+import LogIn from './LogIn'
+import Payment from './Payment'
+import SignUp from './SignUp'
+import Survey from './Survey'
 
 const Pages = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/' element={<Home />} />
-      <Route path='/' element={<Home />} />
-      <Route path='/' element={<Home />} />
-      <Route path='/' element={<Home />} />
-      <Route path='/' element={<Home />} />
-      <Route path='/' element={<Home />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/login' element={<LogIn />} />
+      <Route path='/accountinfo' element={<AccountInfo />} />
+      <Route path='/survey' element={<Survey />} />
+      <Route path='/documentcourse' element={<Home />} />
+      <Route path='/visualcourse' element={<Home />} />
+      <Route path='/payment' element={<Payment />} />
     </Routes>
   )
 }
