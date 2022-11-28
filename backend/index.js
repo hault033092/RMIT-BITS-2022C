7acba8f17a1110677677325c0df327e9d1c7a3b1
Trung Hau Le <letrunghau090298@gmail.com>
2022-11-28 18:32:14 +0700

    add route and middleware index.js

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,13 @@ const cors = require('cors')
 require('dotenv/config')
 
 //Routes
+const userRoute = require('./routes/users.routes')
 
 //Middlewares
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/api/user', userRoute)
 
 //Server Config
 app.listen(8080, () => console.log('Port listened!'))
