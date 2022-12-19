cedab8cb5d3dd19ed52491213728567f7e5474fa
LongJoe2002 <nguyentrongminhlong@gmail.com>
2022-12-19 10:20:49 +0700

    Add component and css for component in ielts text

--- frontend/src/Components/CourseInfo/CourseContentText.jsx
+++ frontend/src/Components/CourseInfo/CourseContentText.jsx
@@ -4,11 +4,20 @@ import Title from "../Title"
 
 const CourseContentText = ({title, content}) => {
     return(
-        <div>
+        <Container>
             <Title title={title}></Title>
             <p>{content}</p>
-        </div>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+`
 
 export default CourseContentText;
