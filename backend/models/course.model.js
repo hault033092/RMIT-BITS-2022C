f7bc1ceb69592eee7a81c3acb1a8123a3e1d0e5e
Trung Hau Le <letrunghau090298@gmail.com>
2023-01-03 13:39:51 +0700

    fix stuffs for real

--- backend/models/course.model.js
+++ backend/models/course.model.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose')
 
 const courseSchema = new mongoose.Schema(
   {
-    courseName: {
+    courseTitle: {
       type: String,
       required: true,
     },
+    courseImg: { type: String },
     courseType: {
+      type: String,
+      required: true,
+    },
+    courseDetail: {
       type: String,
       required: true,
     },
