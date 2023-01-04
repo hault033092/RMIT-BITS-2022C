cd6b876fd0e4a22d23dbfbdc26278574a0a42863
Giang Nhật Khánh (Kayden) <nhatkhanhgiang2512@gmail.com>
2023-01-04 11:17:54 +0700

    fixed homepage layout

--- frontend/src/Components/Carousel/PromotedCarousel.jsx
+++ frontend/src/Components/Carousel/PromotedCarousel.jsx
@@ -1,55 +1,57 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Splide, SplideSlide } from '@splidejs/react-splide'
-import { AutoScroll } from '@splidejs/splide-extension-auto-scroll'
-import '@splidejs/splide/dist/css/splide.min.css'
-import testImg from '../../assets/IELTS_image.png'
-import CourseItem from '../CourseInfo/CourseItem'
+import React from "react"
+import styled from "styled-components"
+import { Splide, SplideSlide } from "@splidejs/react-splide"
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll"
+import "@splidejs/splide/dist/css/splide.min.css"
+import testImg from "../../assets/IELTS_image.png"
+import CourseItem from "../CourseInfo/CourseItem"
 
 const PromotedCarousel = () => {
-  return (
-    <CarouselContainer
-      options={{
-        perPage: 3,
-        arrows: true,
-        pagination: true,
-        gap: '5rem',
-        autoScroll: {
-          pauseOnFocus: true,
-          pauseOnHover: true,
-          rewind: true,
-          speed: 1,
-        },
-      }}
-      extensions={{ AutoScroll }}
-    >
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-    </CarouselContainer>
-  )
+	return (
+		<CarouselContainer
+			options={{
+				perPage: 3,
+				arrows: true,
+				pagination: true,
+				gap: "5rem",
+				autoScroll: {
+					pauseOnFocus: true,
+					pauseOnHover: true,
+					rewind: true,
+					speed: 1,
+				},
+			}}
+			extensions={{ AutoScroll }}
+		>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+		</CarouselContainer>
+	)
 }
 
 const CarouselContainer = styled(Splide)`
-  width: 100%;
-  height: 50vh;
+	width: 100%;
+	height: 50vh;
+	display: flex;
+	align-items: center;
 
-  /* img {
+	/* img {
     width: 150px;
     height: 150px;
   } */
--- frontend/src/Components/Carousel/TextCarousel.jsx
+++ frontend/src/Components/Carousel/TextCarousel.jsx
@@ -1,55 +1,60 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Splide, SplideSlide } from '@splidejs/react-splide'
-import { AutoScroll } from '@splidejs/splide-extension-auto-scroll'
-import '@splidejs/splide/dist/css/splide.min.css'
-import testImg from '../../assets/IELTS_image.png'
-import CourseItem from '../CourseInfo/CourseItem'
+import React from "react"
+import styled from "styled-components"
+import { Splide, SplideSlide } from "@splidejs/react-splide"
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll"
+import "@splidejs/splide/dist/css/splide.min.css"
+import testImg from "../../assets/IELTS_image.png"
+import CourseItem from "../CourseInfo/CourseItem"
 
 const TextCarousel = () => {
-  return (
-    <CarouselContainer
-      options={{
-        perPage: 4,
-        arrows: true,
-        pagination: true,
-        gap: '3rem',
-        autoScroll: {
-          pauseOnFocus: true,
-          pauseOnHover: true,
-          rewind: true,
-          speed: 1,
-        },
-      }}
-      extensions={{ AutoScroll }}
-    >
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-      <SplideSlide>
-        <CourseItem />
-      </SplideSlide>
-    </CarouselContainer>
-  )
+	return (
+		<CarouselContainer
+			options={{
+				perPage: 3,
+				arrows: true,
+				pagination: true,
+				gap: "5rem",
+				autoScroll: {
+					pauseOnFocus: true,
+					pauseOnHover: true,
+					rewind: true,
+					speed: 1,
+				},
+			}}
+			extensions={{ AutoScroll }}
+		>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+			<SplideSlide>
+				<CourseItem />
+			</SplideSlide>
+		</CarouselContainer>
+	)
 }
 
 const CarouselContainer = styled(Splide)`
-  img {
+	width: 100%;
+	height: 50vh;
+	display: flex;
+	align-items: center;
+
+	/* img {
     width: 150px;
     height: 150px;
-  }
+  } */
 `
 
 export default TextCarousel
