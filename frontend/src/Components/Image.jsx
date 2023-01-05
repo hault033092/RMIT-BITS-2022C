552f1e849592e377f14137b97135259aa2f4e9c6
Giang Nhật Khánh (Kayden) <nhatkhanhgiang2512@gmail.com>
2023-01-05 19:09:02 +0700

    slight fix to course images

--- frontend/src/Components/Image.jsx
+++ frontend/src/Components/Image.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
 
 const Image = ({ imgSrc }) => {
-  return <ImageContainer src={imgSrc}></ImageContainer>
+	return <ImageContainer src={imgSrc}></ImageContainer>
 }
 
 const ImageContainer = styled.img`
-  width: 100px;
-  height: 100px;
-  /* aspect-ratio: 1 / 1; */
+	max-width: 6.25rem;
+	aspect-ratio: 1 / 1;
 `
 
 export default Image
