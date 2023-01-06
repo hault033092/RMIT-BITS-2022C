946fa2676f0ee0dda72580d7d598c816ea001438
Giang Nhật Khánh (Kayden) <nhatkhanhgiang2512@gmail.com>
2023-01-06 10:45:38 +0700

    fixed layout bug

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,66 +1,66 @@
-import React from 'react'
-import Pages from './Pages'
-import Header from '../Components/Header/Header'
-import Footer from '../Components/Footer/Footer'
-import styled from 'styled-components'
-import Carousel from '../Components/Carousel/Carousel'
-import PromotedCarousel from '../Components/Carousel/PromotedCarousel'
-import TextCarousel from '../Components/Carousel/TextCarousel'
-import VisualCarousel from '../Components/Carousel/VisualCarousel'
-import CourseItem from '../Components/CourseInfo/CourseItem'
+import React from "react"
+import Pages from "./Pages"
+import Header from "../Components/Header/Header"
+import Footer from "../Components/Footer/Footer"
+import styled from "styled-components"
+import Carousel from "../Components/Carousel/Carousel"
+import PromotedCarousel from "../Components/Carousel/PromotedCarousel"
+import TextCarousel from "../Components/Carousel/TextCarousel"
+import VisualCarousel from "../Components/Carousel/VisualCarousel"
+import CourseItem from "../Components/CourseInfo/CourseItem"
 
 const Home = () => {
-  return (
-    <Container>
-      <PromotedCourses>
-        <h1>Promoted Courses</h1>
-        <PromotedCarousel />
-      </PromotedCourses>
-      <FeaturedText>
-        <h1>Featured Text Courses</h1>
-        <TextCarousel />
-      </FeaturedText>
-      <FeaturedVisual>
-        <h1>Featured Visual Courses</h1>
-        <VisualCarousel />
-      </FeaturedVisual>
-    </Container>
-  )
+	return (
+		<Container>
+			<PromotedCourses>
+				<h1>Promoted Courses</h1>
+				<PromotedCarousel />
+			</PromotedCourses>
+			<FeaturedText>
+				<h1>Featured Text Courses</h1>
+				<TextCarousel />
+			</FeaturedText>
+			<FeaturedVisual>
+				<h1>Featured Visual Courses</h1>
+				<VisualCarousel />
+			</FeaturedVisual>
+		</Container>
+	)
 }
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
 `
 const PromotedCourses = styled.div`
-  height: 50vh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
+	height: 80vh;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
 `
 
 const FeaturedText = styled.div`
-  height: 50vh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  /* padding-top: 10rem; */
+	height: 80vh;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	/* padding-top: 10rem; */
 `
 
 const FeaturedVisual = styled.div`
-  height: 50vh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  /* padding-top: 10rem; */
+	height: 80vh;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	/* padding-top: 10rem; */
 `
 
 export default Home
